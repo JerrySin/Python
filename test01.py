517052b950bfa7df9a648cc6e9d12125ffb1a7bb
JerrySin <Jerry.at.crosssea@gmail.com>
2022-03-08 12:25:54 -0500

    Update test01.py

--- test01.py
+++ test01.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def test_run():
     df = pd.read_csv("Crypto_Resources.csv",delimiter=",")
-    print df #print entire dataframe
+    print(df) #print entire dataframe, python3
 
 
 if __name__ == "__main__":
